c09905aa26b078af6b5f96a8dde0d62d9e9639ad
unknown <asukaru2014@icloud.com>
2019-03-08 16:09:34 +0900

    処理切り分け　外部ファイル化

--- index.php
+++ index.php
@@ -6,29 +6,12 @@
 	$token = Security::makeCsrf();
 	$_SESSION['csrf_token'] = $token;
 	$blog = new blog();
+
+	// 2019/03/08　　header.php 読み込み
+	include_once 'header.php';
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-	<!--  BootStrap 使用 -->
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-	<title>blog System</title>
-	<!-- 2019/03/08 css　追加 -->
-	<link rel="stylesheet" href="css/main.css">
-</head>
-<body>
-	<div id="sideBar">
-		<ul>
-			<li><a href="">投稿一覧</a></li>
-			<li><a href="">カテゴリ設定</a></li>
-			<li><a href=""></a></li>
-			<li><a href=""></a></li>
-		</ul>
-	</div><!-- // sideBar -->
+<?php include_once 'sideBar.php'; ?>
 
 	<div id="main">
 							<!-- 2019/03/08 Limit区を使った取得制限 追加 -->
---
+++ sideBar.php
@@ -0,0 +1,8 @@
+	<div id="sideBar">
+		<ul>
+			<li><a href="">投稿一覧</a></li>
+			<li><a href="">カテゴリ設定</a></li>
+			<li><a href=""></a></li>
+			<li><a href=""></a></li>
+		</ul>
+	</div><!-- // sideBar -->
